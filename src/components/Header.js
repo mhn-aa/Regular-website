18abc602521e7274f5d7105d9606089e1f43d098
mhn-aa <mahanmirjamali@gmail.com>
2021-12-25 17:53:20 +1100

    Header First Step

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div className="et-dev-header">
+      <div className="logo"></div>
+      <h1>ET Dev Group</h1>
+    </div>
+  );
+};
+
+export default header;
