2544dea4119219d5f85e686a7a65c4e7635a9687
Saeed <saeed_et90@yahoo.com>
2021-12-29 23:06:59 +1100

    RW-15 bg logo response

--- src/Pages/Saeed/Saeed.js
+++ src/Pages/Saeed/Saeed.js
@@ -10,22 +10,82 @@ const Saeed = () => {
       </div>
       <div className="saeed__colorsContainer">
         <div className="saeed__colorsContainer__row__1 ">
-          <div className="saeed__colorBox saeed__topRow saeed_color5"></div>
-          <div className="saeed__colorBox saeed__topRow saeed_color6"></div>
-          <div className="saeed__colorBox saeed__topRow saeed_color7"></div>
+          {/* 5 */}
+          <div className="saeed__colorBox saeed__topRow">
+            <img
+              alt="SQL"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sql/sql.png"
+            />
+          </div>
+          {/* 6 */}
+          <div className="saeed__colorBox saeed__topRow">
+            <img
+              alt="CSS3"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
+            />
+          </div>
+          {/* 7 */}
+          <div className="saeed__colorBox saeed__topRow">
+            <img
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"
+              alt="html5"
+            />
+          </div>
         </div>
         <div className="saeed__colorsContainer__row__2">
-          <div className="saeed__colorBox saeed_color4"></div>
-          <div className="saeed__colorBox saeed_color8"></div>
+          {/* 4 */}
+          <div className="saeed__colorBox ">
+            <img
+              alt="JavaScript"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+            />
+          </div>
+          {/* 8 */}
+          <div className="saeed__colorBox ">
+            <img
+              alt="Typescript"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
+            />
+          </div>
         </div>
         <div className="saeed__colorsContainer__row__2">
-          <div className="saeed__colorBox saeed_color3"></div>
-          <div className="saeed__colorBox saeed_color9"></div>
+          {/* 3 */}
+          <div className="saeed__colorBox">
+            <img
+              alt="React"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+            />
+          </div>
+          {/* 9 */}
+          <div className="saeed__colorBox ">
+            <img
+              alt="GraphQL"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/graphql/graphql.png"
+            />
+          </div>
         </div>
         <div className="saeed__colorsContainer__row__1">
-          <div className="saeed__colorBox saeed__bottomRow saeed_color2"></div>
-          <div className="saeed__colorBox saeed__bottomRow saeed_color1"></div>
-          <div className="saeed__colorBox saeed__bottomRow saeed_color10"></div>
+          {/* 2 */}
+          <div className="saeed__colorBox saeed__bottomRow">
+            <img
+              alt="Node"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
+            />
+          </div>
+          {/* 1 */}
+          <div className="saeed__colorBox saeed__bottomRow ">
+            <img
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"
+              alt="html5"
+            />
+          </div>
+          {/* 10 */}
+          <div className="saeed__colorBox saeed__bottomRow ">
+            <img
+              alt="postgresql"
+              src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/postgresql/postgresql.png"
+            />
+          </div>
         </div>
       </div>
       <div className="saeed__container">
