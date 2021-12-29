f863d5c1e2f6243a9e7268b39cb54dec0b80ca54
Saeed <saeed_et90@yahoo.com>
2021-12-29 21:39:37 +1100

    RW-15 bg animation

--- src/Pages/Saeed/Saeed.js
+++ src/Pages/Saeed/Saeed.js
@@ -5,8 +5,15 @@ import "./Saeed.css";
 const Saeed = () => {
   return (
     <div className="saeed">
-      <Link to="/">{"<- "} back</Link>
-      <h1>This is Saeed's page</h1>
+      <div className="saeed__link">
+        <Link to="/">🔙 &#128281;</Link>
+      </div>
+      <div className="saeed__container">
+        <img
+          src="https://avatars.githubusercontent.com/u/39290832?v=4"
+          alt="saeed_pic"
+        />
+      </div>
     </div>
   );
 };
